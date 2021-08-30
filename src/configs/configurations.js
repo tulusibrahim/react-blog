ea39e06e3181add02f14e7dd8739d7b944fda7fd
tulusibrahim <tulusibrahim20@gmail.com>
2021-08-31 00:40:46 +0700

    change config

--- src/configs/configurations.js
+++ src/configs/configurations.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 let { v4: uuidv4 } = require('uuid')
 
-const supabase = createClient(process.env.URL, process.env.KEY)
+const supabase = createClient(process.env.REACT_APP_VERCEL_URL, process.env.REACT_APP_VERCEL_KEY)
 
 export {
     supabase,
