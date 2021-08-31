d43c47084599332d7293188ab3d59b10ea244c2b
tulusibrahim <tulusibrahim20@gmail.com>
2021-08-31 11:01:14 +0700

    undo env variable

--- src/configs/configurations.js
+++ src/configs/configurations.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-require('dotenv').config()
 let { v4: uuidv4 } = require('uuid')
 
-const supabase = createClient(process.env.REACT_APP_VERCEL_URL, process.env.REACT_APP_VERCEL_KEY)
+const supabase = createClient("https://bbgnpwbarxehpmmnyfgq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzE4NzU2NywiZXhwIjoxOTQyNzYzNTY3fQ.HtwezwLxcXQEMTOl-vH6b9SszRtByhX1FSF-dOEqrBc")
 
 export {
     supabase,
