5e34a2029f158f3a8e57911483bee06f421c3a9a
tulusibrahim <tulusibrahim20@gmail.com>
2021-08-31 10:47:45 +0700

    change config

--- src/configs/configurations.js
+++ src/configs/configurations.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 require('dotenv').config()
 let { v4: uuidv4 } = require('uuid')
 
-const supabase = createClient(process.env.REACT_APP_URL, process.env.REACT_APP_KEY)
+const supabase = createClient(process.env.REACT_APP_VERCEL_URL, process.env.REACT_APP_VERCEL_KEY)
 
 export {
     supabase,
