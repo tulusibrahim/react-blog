9c1878fb5eecc0b368f4aa04cc8ba674ff3a194a
tulusibrahim <tulusibrahim20@gmail.com>
2021-08-31 10:01:06 +0700

    change config

--- src/configs/configurations.js
+++ src/configs/configurations.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 let { v4: uuidv4 } = require('uuid')
 
-const supabase = createClient(process.env.URL_APP, process.env.KEY_APP)
+const supabase = createClient(URL_APP, KEY_APP)
 
 export {
     supabase,
