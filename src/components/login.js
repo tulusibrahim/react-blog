5513a1b85259612c86c4d4dac617efc4e4675b7a
tulusibrahim <tulusibrahim20@gmail.com>
2022-05-26 13:17:06 +0700

    fix: separate between logic and UI for the login component

--- src/components/login.js
+++ src/components/login.js
@@ -1,71 +1,9 @@
-import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { supabase } from "../configs/configurations";
-import swal from 'sweetalert';
-import { Input, Text, InputGroup, FormControl, Button, InputRightElement, Box, Flex } from "@chakra-ui/react"
+import { Input, Text, InputGroup, Button, InputRightElement, Box, Flex } from "@chakra-ui/react"
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import useLogin from "./loginhooks";
 
 const Login = (props) => {
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [login, setlogin] = useState('')
-    const [show, setShow] = useState(false)
-    let history = useHistory()
-
-    const signUp = async (e) => {
-        e.preventDefault()
-
-        const { user, session, error } = await supabase.auth.signUp({
-            email: email,
-            password: password,
-        })
-        console.log(error)
-
-        if (error) {
-            swal("Failed to sign up, please try again", {
-                icon: "warning",
-            });
-        }
-        else {
-            history.push('/')
-            // document.location.reload()
-        }
-    }
-
-    const logIn = async (e) => {
-        e.preventDefault()
-        e.target.reset()
-
-        const { user, session, error } = await supabase.auth.signIn({
-            email: email,
-            password: password,
-        })
-        if (error) {
-            // alert("Failed to sign in")
-            swal("Failed to sign in. Please try again", {
-                icon: "error",
-            });
-        }
-        else {
-            history.push('/')
-            // document.location.reload()
-        }
-    }
-
-    const loginWithGithub = async () => {
-        const { user, session, error } = await supabase.auth.signIn({
-            // provider can be 'github', 'google', 'gitlab', or 'bitbucket'
-            provider: 'github'
-        })
-    }
-
-    useEffect(async () => {
-        let isLogin = await supabase.auth.session()
-        if (isLogin !== null) {
-            history.push('/')
-        }
-    }, [])
+    let { login, show, signUp, logIn, loginWithGithub, setEmail, setPassword, setlogin, setShow } = useLogin()
 
     return (
         <Flex direction="column" alignItems="center" justify="space-evenly" w="100%" h="90vh">
@@ -84,8 +22,6 @@ const Login = (props) => {
                                     </InputRightElement>
                                 </InputGroup>
                                 <Box textAlign="center" width={['93%', '70%', '50%', '30%']}>
-                                    {/* <p>Have an account? Login <a style={{ cursor: 'pointer', color: '#536f8d' }} onClick={() => setlogin('true')}>here!</a></p> */}
-                                    {/* <button>Sign Up</button> */}
                                     <Text color="white">Have an account? Login
                                         <a style={{ cursor: 'pointer', color: '#536f8d' }} onClick={() => setlogin('true')}> here!</a>
                                     </Text>
